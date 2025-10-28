02a8770d4fc98afe8b4854977c75b9268ae08662
Edelweiss Gevorgyan <edelweissgevorgyan@Edelweisss-MacBook-Air.local>
2025-10-28 16:47:02 +0400

    Add HW2 folder with all homework 2 files

---
+++ HW2/configs.py
@@ -0,0 +1,5 @@
+BANDIT_ARMS = [1, 2, 3, 4]  # True means
+N_TRIALS = 20000            # Number of trials
+EPSILON = 1.0               # Initial epsilon for E-Greedy
+PRECISION = 1.0             # For Thompson Sampling
+RANDOM_SEED = 42            # For reproducibility
